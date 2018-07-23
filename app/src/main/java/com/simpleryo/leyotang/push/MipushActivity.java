1e8c8ed87212fcb34bdc08f3aea6b44600cfddbf
774293015@qq.com <hl19940921>
2018-07-23 09:09:13 +0800

    修改了华为推送

--- app/src/main/java/com/simpleryo/leyotang/push/MipushActivity.java
+++ app/src/main/java/com/simpleryo/leyotang/push/MipushActivity.java
@@ -14,19 +14,22 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Map;
+
 /**
+ * @author huanglei
  * @ClassNname：MipushtActivity.java
  * @Describe 华为系统推送
- * @author huanglei
  * @time 2018/7/9 10:12
  */
-public class MipushtActivity extends UmengNotifyClickActivity {
-    private static String TAG = MipushtActivity.class.getName();
+public class MipushActivity extends UmengNotifyClickActivity {
+    private static String TAG = MipushActivity.class.getName();
+
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.activity_splash);
     }
+
     @Override
     public void onMessage(Intent intent) {
         super.onMessage(intent);  //此方法必须调用，否则无法统计打开数
@@ -39,26 +42,23 @@ public class MipushtActivity extends UmengNotifyClickActivity {
 //                payComplete 支付完成
 //                sysMessage 系统消息
             if (extrs.get("type").equalsIgnoreCase("courseClass")) {
-                Intent courseClass = new Intent(MipushtActivity.this, MainActivity.class);
+                Intent courseClass = new Intent(MipushActivity.this, MainActivity.class);
                 courseClass.putExtra("type", "push");
                 courseClass.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(courseClass);
                 finish();
-            }
-            if (extrs.get("type").equalsIgnoreCase("payComplete")) {
-                Intent payComplete = new Intent(MipushtActivity.this, MainActivity.class);
+            } else if (extrs.get("type").equalsIgnoreCase("payComplete")) {
+                Intent payComplete = new Intent(MipushActivity.this, MainActivity.class);
                 payComplete.putExtra("type", "push");
                 payComplete.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(payComplete);
                 finish();
-            }
-            if (extrs.get("type").equalsIgnoreCase("sysMessage")) {
-                Intent sysMessage = new Intent(MipushtActivity.this, MyMsgActivity.class);
+            } else if (extrs.get("type").equalsIgnoreCase("sysMessage")) {
+                Intent sysMessage = new Intent(MipushActivity.this, MyMsgActivity.class);
                 sysMessage.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(sysMessage);
                 finish();
             }
-
         } catch (JSONException e) {
             e.printStackTrace();
         }
